603c6e93275fdda6def28c02ac87880051ad6640
chris <chris314159@gmail.com>
2015-01-18 09:32:45 -0600

    Testing linode git setup

--- ket.py
+++ ket.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "heyman8"
-    #return render_template(index.html)
+    return render_template("main.html")
 
 if __name__ == "__main__":
     app.debug = True
