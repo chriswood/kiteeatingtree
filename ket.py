7d3e33a49654f2dbebdbeab8ab7405b0f50ad898
chris <chris314159@gmail.com>
2015-01-18 21:21:29 -0600

    Adding a basic layout and login form

--- ket.py
+++ ket.py
@@ -3,10 +3,12 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("main.html")
+import ket
+import views
+
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
+
+
