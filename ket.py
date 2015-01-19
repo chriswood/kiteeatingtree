a27cf8be4b49bdea4bc169ef64e47aa75802d8f4
chris <chris314159@gmail.com>
2015-01-19 11:48:35 -0600

    Adding a basic layout and stuff, I'm still trying to remember how jinja/flask syntax.

--- ket.py
+++ ket.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__.split('.')[0])
 
 import ket
 import views
-
+from views import *
 
 if __name__ == "__main__":
     app.debug = True
--- views.py
+++ views.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, url_for
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__.split('.')[0])
 
 @app.route("/")
 def index():
-    return render_template("main.html")
+    image = url_for('static', filename='images/elephants.jpg')
+    return render_template("main.html", image=image)
