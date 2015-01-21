ea194c73c50594d791a024306aede6f9e5997b59
chris <chris314159@gmail.com>
2015-01-20 20:29:43 -0600

    Adding a header and some basic style.

--- views.py
+++ views.py
@@ -5,5 +5,6 @@ app = Flask(__name__.split('.')[0])
 
 @app.route("/")
 def index():
-    image = url_for('static', filename='images/elephants.jpg')
-    return render_template("main.html", image=image)
+    header_url = url_for('static', filename='images/header.png')
+    logged_in = True
+    return render_template("main.html", header=header_url, logged_in=logged_in)
