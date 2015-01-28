62ac22a5706fb6d241c78c9ae6d16388b65af5ed
chris <chris314159@gmail.com>
2015-01-28 17:46:56 -0600

    Adding in jquery growl, but not using yet.
    I may remove it later if it isn't useful.

---
+++ static/js/notifications.js
@@ -0,0 +1,6 @@
+$(function() {
+    $.growl({ title: "Growl", message: "The kitten is awake!" });
+    $.growl.error({ message: "The kitten is attacking!" });
+    $.growl.notice({ message: "The kitten is cute!" });
+    $.growl.warning({ message: "The kitten is ugly!" });
+});
